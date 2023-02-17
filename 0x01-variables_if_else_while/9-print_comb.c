140b43206aac22e84b7cb2c0203c9460be1c4ec1
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-02-17 06:29:18 -0800

    refactored teh 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,17 +12,10 @@ for (i = 0; i < 10; i++)
 for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
-putchar(',');
-putchar(' ');
 putchar(j + '0');
-if (i == 8 && j == 9)
-{
-continue;
-}            
-putchar(',');
-putchar(' ');
+putchar((i == 8 && j == 9) ? '\n' : ',');
+putchar((i == 8 && j == 9) ? '\0' : ' ');
 }
 }
-putchar('\n');
 return (0);
 }
