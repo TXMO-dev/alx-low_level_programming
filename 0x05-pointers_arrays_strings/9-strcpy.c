2fafd9c0eda01f5b294e2e7ae9a1526480049cf4
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-02-27 13:51:57 -0800

    fixed line over 80 characters

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
-* _strcpy - Copies the string pointed to by src, including the terminating null byte,
+* _strcpy - Copies the string pointed to by src,
 * to the buffer pointed to by dest.
 *
 * @dest: A buffer to copy the string to.
