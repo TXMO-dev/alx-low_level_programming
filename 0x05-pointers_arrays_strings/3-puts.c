6e0e59e63a8f8d492081790825f56af1a97ff347
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-02-27 05:52:00 -0800

    puts command with pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+* _puts - Prints a string to the standard output followed by a new line.
+*
+* @str: Pointer to the string.
+*/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+}
