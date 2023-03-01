5513555f8933f80373f158dc3c7b11ac1018e401
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-01 13:58:17 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+* _strcmp - Compares two strings.
+*
+* @s1: The first string to be compared.
+* @s2: The second string to be compared.
+*
+* Return: An integer less than, equal to, or greater than zero if s1 is found,
+* respectively, to be less than, to match, or be greater than s2.
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+for (i = 0; s1[i] == s2[i]; i++)
+{
+if (s1[i] == '\0')
+{
+return (0);
+}
+}
+return ((int)(s1[i] - s2[i]));
+}
