d531419a92ed1af351996aa1906157b3bafefe2b
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-06 01:17:22 -0800

    fixed errors on string char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,9 @@
 /**
-*_strchr - locates a character in a string
-*@s: pointer to string to be searched
-*@c: character to be located
-*
-*Return: pointer to the first occurrence of the character c in the string s,
-*or NULL if the character is not found
+* _strchr - locates a character in a string
+* @s: pointer to string to be searched
+* @c: character to be located
+* Return: pointer to the first occurrence of the character c in the string s,
+* or NULL if the character is not found
 */
 char *_strchr(char *s, char c)
 {
