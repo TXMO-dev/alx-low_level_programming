19e70c2a6a7a69d4181ca68ee554a3df37cb25e8
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-08 01:17:42 -0800

    added prime number code

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,16 @@
 /**
-* is_prime_number - checks if a given number is a prime number
+* is_divisible - checks if n is divisible by i
+* @n: the number to check
+* @i: the divisor to check
+*
+* Return: 1 if n is divisible by i, 0 otherwise
+*/
+int is_divisible(int n, int i);
+/**
+* is_prime_number - checks if a given number is prime
 * @n: the number to check
 *
-* Return: 1 if the number is prime, otherwise 0
+* Return: 1 if n is prime, 0 otherwise
 */
 int is_prime_number(int n)
 {
@@ -10,21 +18,32 @@ if (n <= 1)
 {
 return (0);
 }
-else if (n <= 3)
+if (n == 2)
 {
 return (1);
 }
-else if (n % 2 == 0 || n % 3 == 0)
+if (n % 2 == 0)
 {
 return (0);
 }
-int i;
-for (i = 5; i * i <= n; i += 6)
-{
-if (n % i == 0 || n % (i + 2) == 0)
-{
-return (0);
+return (is_divisible(n, 3));
 }
-}
+/**
+* is_divisible - checks if n is divisible by i
+* @n: the number to check
+* @i: the divisor to check
+*
+* Return: 1 if n is divisible by i, 0 otherwise
+*/
+int is_divisible(int n, int i)
+{
+if (i * i > n)
+{
 return (1);
+}
+if (n % i == 0)
+{
+return (0);
+}
+return (is_divisible(n, i + 2));
 }
