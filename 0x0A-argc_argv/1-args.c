ae1c0d4bcd28cbfc640144fd79df8dea4fecc882
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-10 14:58:10 -0800

    this is the low level programming

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+
 /**
-* main - prints the number of arguments passed into it
-* @argc: the number of arguments
-* @argv: an array of strings containing the arguments
-*
-* Return: 0 if successful
-*/
+ * main - prints the number of arguments passed into it
+ * @argc: the number of arguments
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0 if successful
+ */
 int main(int argc, char *argv[])
 {
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
 printf("%d\n", argc - 1);
+}
 return (0);
 }
 
