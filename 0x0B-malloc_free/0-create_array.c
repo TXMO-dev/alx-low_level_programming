81f546f1435108b192f97f6cb2aabdffbd94a66d
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-14 13:48:46 -0700

    first malloc test

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+* create_array - creates an array of chars
+* @size: size of array
+* @c: specific char to initialize array with
+*
+* Return: pointer to the array, or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (NULL);
+}
+char *array = malloc(size * sizeof(char));
+if (array == NULL)
+{
+return (NULL);
+}
+for (unsigned int i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
+}
