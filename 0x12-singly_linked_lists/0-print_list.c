1a8c4c21a245064d627db546bc2a20c56d398ec7
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-31 13:32:32 -0700

    first singly list test

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to head of the list
+*
+* Return: number of nodes in the list
+*/
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
+h = h->next;
+count++;
+}
+return (count);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+size_t print_list(const list_t *h);
+#endif /* MAIN_H */
