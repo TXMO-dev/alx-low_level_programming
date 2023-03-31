6616225089396942e4f71df5035e0be3421160e7
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-31 14:47:29 -0700

    bonus qa

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* premain - function that prints
+*
+* Return: void
+*/
+void __attribute__ ((constructor)) premain(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
