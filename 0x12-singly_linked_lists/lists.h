105010243cbbae5b87a18b645e7ff4a4cf4e4e76
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-03-31 13:54:08 -0700

    sorted out lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
+/**
+* struct list_s - singly linked list node
+* @str: string stored in the node
+* @len: length of the string
+* @next: pointer to the next node
+*
+* Description: This struct defines a node in a singly linked list. It contains
+* a string and its length, as well as a pointer to the next node in the list.
+*/
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif /* MAIN_H */
