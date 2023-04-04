5ef152a389cd77d98bca73a22ea5d6a411d18bcf
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-04-04 15:46:24 -0700

    fixed betty issues

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,18 +2,17 @@
 #define MAIN_H
 #include <stdio.h>
 /**
-* struct list_s - singly linked list node
-* @str: string stored in the node
-* @len: length of the string
-* @next: pointer to the next node
+*struct listint_s - singly linked list
+*@n: integer
+*@next: points to the next node
 *
-* Description: This struct defines a node in a singly linked list. It contains
-* a string and its length, as well as a pointer to the next node in the list.
+*Description: singly linked list node structure
+*
 */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 #endif /* MAIN_H */
