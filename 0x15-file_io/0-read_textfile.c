f93837e3298e61098f474d8b7898a8f70efc828c
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-04-10 13:04:49 -0700

    this is the changes in 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,21 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
 * read_textfile - reads a text file and prints it to the POSIX standard output
-* @filename: name of the file to read
-* @letters: number of letters to read and print
+* @filename: name of the file to be read
+* @letters: maximum number of characters to be read and printed
 *
-* Return: the actual number of letters it could read and print, or 0 on failure
+* Return: actual number of letters it could read and print
+*         0 if the file can not be opened or read, or if write fails or
+*         does not write the expected amount of bytes
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t bytes_read, bytes_written;
-char buffer[1024];
+int fd, bytes_read;
+ssize_t total_bytes_read = 0;
+char buf[1024];
 if (!filename)
 {
 return (0);
@@ -20,18 +25,20 @@ if (fd == -1)
 {
 return (0);
 }
-bytes_read = read(fd, buffer, sizeof(buffer));
-if (bytes_read == -1)
+while ((bytes_read = read(fd, buf, 1024)) > 0 && letters > 0)
+{
+if (write(STDOUT_FILENO, buf, bytes_read) != bytes_read)
 {
 close(fd);
 return (0);
 }
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-close(fd);
-return (0);
+total_bytes_read += bytes_read;
+letters -= bytes_read;
 }
 close(fd);
-return (bytes_written);
+if (bytes_read < 0)
+{
+return (0);
+}
+return (total_bytes_read);
 }
