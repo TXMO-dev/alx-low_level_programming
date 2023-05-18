d44f3b620531796be1caeb9a51ac13791a2c6cef
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-18 14:49:14 -0700

    add noteint

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 typedef struct dlistint_s
 {
 int n;
