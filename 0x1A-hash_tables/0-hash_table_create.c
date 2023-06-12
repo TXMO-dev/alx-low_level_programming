d5dcc3a67db9c590a91e0830d5d301bb570755c7
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-12 16:31:14 -0700

    refactor hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,33 +1,37 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_create - Creates a hash table
-* @size: The size of the array
-*
-* Return: A pointer to the newly created hash table,
-*         or NULL if something went wrong
-*/
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table,
+ *         or NULL if something went wrong.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table;
+hash_table_t *ht;
+hash_node_t **array;
 unsigned long int i;
-/* Allocate memory for the hash table */
-hash_table = malloc(sizeof(hash_table_t));
-if (hash_table == NULL)
+if (size == 0)
 {
 return (NULL);
 }
-/* Allocate memory for the array of nodes */
-hash_table->array = malloc(sizeof(hash_node_t *) * size);
-if (hash_table->array == NULL)
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
 {
-free(hash_table);
 return (NULL);
 }
-/* Initialize each array element to NULL */
+array = malloc(sizeof(hash_node_t *) * size);
+if (array == NULL)
+{
+free(ht);
+return (NULL);
+}
+ht->size = size;
+ht->array = array;
 for (i = 0; i < size; i++)
 {
-hash_table->array[i] = NULL;
+ht->array[i] = NULL;
 }
-hash_table->size = size;
-return (hash_table);
+return (ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,3 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-
-#include <stdlib.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -31,20 +26,3 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
-
-/* Function to create a hash table */
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-
-#endif /* HASH_TABLES_H */
